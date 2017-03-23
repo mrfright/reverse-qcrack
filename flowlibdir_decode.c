cf155abe2298460158d71df9b5d4de6302b2beaf
Eric Faehnrich <misterfright@gmail.com>
2017-03-23 15:37:50 -0400

    extract files

--- flowlibdir_decode.c
+++ flowlibdir_decode.c
@@ -11,24 +11,31 @@ struct flowlibdir_entry {
 #include <string.h>
 
 void flowlibdir(void) {
-    FILE* fp;
+    FILE* flowlibdir;
+    FILE* flowliblib;
+    char file_buff[1024*1024*6];//6MB file buffer, bigger than whole flowlib.lib file
     struct flowlibdir_entry entries[1000];
     int num_entries = 0;
-    
-    if((fp = fopen("flowlib.dir", "rb")) == NULL) {
+
+    //does it matter if I pass b for binary to fopen?
+    if((flowlibdir = fopen("flowlib.dir", "rb")) == NULL) {
         printf("can't open flowlib.dir\n");
         return;
+    }
+
+    if((flowliblib = fopen("flowlib.lib", "rb")) == NULL) {
+        printf("can't open flowlib.lib\n");
     }
 
     int num_objs_read;
 
     int thing;
 
-    num_objs_read = fread(&thing, 4, 1, fp);
+    num_objs_read = fread(&thing, 4, 1, flowlibdir);
     char c;
-    while((c=getc(fp)) != EOF && num_entries <= 1000) {
-        ungetc(c, fp);
-        num_objs_read = fread(&entries[num_entries], 12, 1, fp);
+    while((c=getc(flowlibdir)) != EOF && num_entries <= 1000) {
+        ungetc(c, flowlibdir);
+        num_objs_read = fread(&entries[num_entries], 12, 1, flowlibdir);
         if(num_objs_read != 1) {
             printf("didn't get 1 object for file name %s as expected:%d\n",
                    entries[num_entries].file_name,
@@ -36,19 +43,42 @@ void flowlibdir(void) {
         }
         entries[num_entries].file_name[12] = '\0';
 
-        num_objs_read = fread(&entries[num_entries].something, 4, 1, fp);
+        num_objs_read = fread(&entries[num_entries].something, 4, 1, flowlibdir);
         if(num_objs_read != 1) {
             printf("didn't get 1 object for something as expected:%d\n", num_objs_read);
         }
 
-        num_objs_read = fread(&entries[num_entries].offset, 4, 1, fp);
+        num_objs_read = fread(&entries[num_entries].offset, 4, 1, flowlibdir);
         if(num_objs_read != 1) {
             printf("didn't get 1 object for offset as expected:%d\n", num_objs_read);
         }
 
-        num_objs_read = fread(&entries[num_entries].size, 4, 1, fp);
+        num_objs_read = fread(&entries[num_entries].size, 4, 1, flowlibdir);
         if(num_objs_read != 1) {
             printf("didn't get 1 object for size as expected:%d\n", num_objs_read);
+        }
+
+        if(flowliblib) {
+            FILE* extracted_file;
+        
+            if((extracted_file = fopen(entries[num_entries].file_name, "wb")) != NULL) {
+                //get the file out of flowlib.lib
+                if(fread(file_buff, entries[num_entries].size, 1, flowliblib) != 1) {
+                    printf("didn't get 1 object extracted from flowlib.lib for %s\n",
+                           entries[num_entries].file_name);
+                }
+                
+                //write to the file
+                if(fwrite(file_buff, entries[num_entries].size, 1, extracted_file) != 1) {
+                    printf("didn't write 1 object of extracted file %s\n",
+                           entries[num_entries].file_name);
+                }
+            }
+            else {
+                printf("could not open file %s to write to\n", entries[num_entries].file_name);
+            }
+
+            fclose(extracted_file);
         }
 
         num_entries++;
